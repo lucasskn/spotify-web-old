1d289e6a8f65e58a8dcb36de36585caef9db08e1
Lucas Klein <77367117+lucasskn@users.noreply.github.com>
2023-04-16 12:28:28 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const backgroundmain = document.querySelector("body");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const card = document.querySelectorAll(".card");
+  card.forEach(function (hover) {
+    hover.addEventListener("mouseover", function (event) {
+      backgroundmain.style.background =
+        "linear-gradient(#6e6e6e -75%,#121212 40%)";
+      backgroundmain.style.transition = "all 0.5s";
+    });
+    hover.addEventListener("mouseout", function (event) {
+      backgroundmain.style.background = "linear-gradient(#222222, #121212)";
+    });
+  });
+});
+
+var cards = document.querySelectorAll(".card");
+
+// Adicione um ouvinte de eventos para o evento mouseover em cada div .card
+cards.forEach(function (card) {
+  card.addEventListener("mouseover", function () {
+    // Selecione todos os elementos ion-icon dentro da div .card atual
+    var icons = card.querySelectorAll("ion-icon");
+
+    // Defina a opacidade de todos os ion-icons para 0.5 (50%)
+    icons.forEach(function (icon) {
+      icon.style.opacity = "1";
+    });
+  });
+  card.addEventListener("mouseout", function () {
+    // Selecione todos os elementos ion-icon dentro da div.card atual
+    var icons = card.querySelectorAll("ion-icon");
+    icons.forEach(function (icon) {
+      icon.style.opacity = "0";
+    });
+  });
+});
